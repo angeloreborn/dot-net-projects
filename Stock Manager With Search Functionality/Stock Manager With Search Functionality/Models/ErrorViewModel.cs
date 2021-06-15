e24335bfb1b10bd68a398610e65c7e2ff1d26565
angeloreborn <angelowolffreborn@gmail.com>
2021-06-15 15:00:50 +0200

    Added Project stock-manage-with-search-functionality

---
+++ Stock Manager With Search Functionality/Stock Manager With Search Functionality/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stock_Manager_With_Search_Functionality.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Stock Manager With Search Functionality/Stock Manager With Search Functionality/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock_Manager_With_Search_Functionality.Models
+{
+    // Minimized (No Annotations) -> GoTo Course On Annotations
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+
+        public double Price { get; set; }
+
+        public int Supplier { get; set; }
+
+    }
+}
