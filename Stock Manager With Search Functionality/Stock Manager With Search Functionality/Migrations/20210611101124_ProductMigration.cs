f8d24a9900f8e9d63827937204c717665d807fab
angeloreborn <angelowolffreborn@gmail.com>
2021-06-15 14:37:50 +0200

    Added Project stock-manager-with-search-functionality
    Readme contains the project details.

---
+++ Stock Manager With Search Functionality/Stock Manager With Search Functionality/Migrations/20210611101124_ProductMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stock_Manager_With_Search_Functionality.Migrations
+{
+    public partial class ProductMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Catagory = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Supplier = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ Stock Manager With Search Functionality/Stock Manager With Search Functionality/Models/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock_Manager_With_Search_Functionality.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public DateTime DateCreated { get; set; }
+
+
+    }
+}
