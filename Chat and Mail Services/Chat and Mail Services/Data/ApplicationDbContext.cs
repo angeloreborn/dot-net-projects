c210d17baf6478da0cf3831cb821f8c4b667b207
angeloreborn <angelowolffreborn@gmail.com>
2021-06-21 10:25:58 +0200

    Create Project Chat and Mail Services

---
+++ Chat and Mail Services/Chat and Mail Services/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat_and_Mail_Services.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
