adb321bdf7c0821b652aff07fbb9d02c64da5264
angeloreborn <angelowolffreborn@gmail.com>
2021-06-22 11:17:26 +0200

    Remove GlobalChatsController scaffolded code
    +Added authorization to Index

--- Chat and Mail Services/Chat and Mail Services/Controllers/GlobalChatsController.cs
+++ Chat and Mail Services/Chat and Mail Services/Controllers/GlobalChatsController.cs
@@ -9,6 +9,7 @@ using Chat_and_Mail_Services.Data;
 using Chat_and_Mail_Services.Models;
 using System.Net.WebSockets;
 using Chat_and_Mail_Services.Middlewares;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Chat_and_Mail_Services.Controllers
 {
@@ -24,138 +25,11 @@ namespace Chat_and_Mail_Services.Controllers
 
 
         // GET: GlobalChats
-        public async Task<IActionResult> Index()
+        [Authorize]
+        public IActionResult Index()
         {
             return View();
         }
 
-
-
-
-
-        // GET: GlobalChats/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var globalChat = await _context.GlobalChat
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (globalChat == null)
-            {
-                return NotFound();
-            }
-
-            return View(globalChat);
-        }
-
-        // GET: GlobalChats/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: GlobalChats/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UserId,MessageContent,TimeStamp")] GlobalChat globalChat)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(globalChat);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(globalChat);
-        }
-
-        // GET: GlobalChats/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var globalChat = await _context.GlobalChat.FindAsync(id);
-            if (globalChat == null)
-            {
-                return NotFound();
-            }
-            return View(globalChat);
-        }
-
-        // POST: GlobalChats/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,MessageContent,TimeStamp")] GlobalChat globalChat)
-        {
-            if (id != globalChat.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(globalChat);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!GlobalChatExists(globalChat.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(globalChat);
-        }
-
-        // GET: GlobalChats/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var globalChat = await _context.GlobalChat
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (globalChat == null)
-            {
-                return NotFound();
-            }
-
-            return View(globalChat);
-        }
-
-        // POST: GlobalChats/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var globalChat = await _context.GlobalChat.FindAsync(id);
-            _context.GlobalChat.Remove(globalChat);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool GlobalChatExists(int id)
-        {
-            return _context.GlobalChat.Any(e => e.Id == id);
-        }
     }
 }
